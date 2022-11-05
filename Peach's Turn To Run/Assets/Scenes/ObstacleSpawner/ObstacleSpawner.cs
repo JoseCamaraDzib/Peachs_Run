6c68031e5008d0e4e55c25281b20a3b2108d7eb4
JoseCamaraDzib <josecamara2001@yahoo.com>
2022-11-04 17:40:05 -0700

    Quick commit, not sure if my changes saved

--- Peach's Turn To Run/Assets/Scenes/ObstacleSpawner/ObstacleSpawner.cs
+++ Peach's Turn To Run/Assets/Scenes/ObstacleSpawner/ObstacleSpawner.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Obstacles : MonoBehaviour
+/*public class Obstacles : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
@@ -15,4 +15,4 @@ public class Obstacles : MonoBehaviour
     {
         
     }
-}
+}*/
