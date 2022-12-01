d94dfad561f4fe80c4d0374852cb76ee3c31c172
pandabunnz <115520957+pandabunnz@users.noreply.github.com>
2022-11-30 23:30:06 -0800

    collision and sfx

--- Peach's Turn To Run/Assets/Scenes+Scripts/Player/PlayerController.cs
+++ Peach's Turn To Run/Assets/Scenes+Scripts/Player/PlayerController.cs
@@ -9,23 +9,26 @@ public class PlayerController : MonoBehaviour
 
     [SerializeField]
     public float leftRightSpeed;
+    static public bool canMove = true;
 
     private void Update()
     {
         transform.Translate(Vector3.forward * Time.deltaTime * moveSpeed, Space.World);
-
-        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+        if (canMove == true)
         {
-            if (this.gameObject.transform.position.x > LevelBoundary.leftSide)
+            if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
             {
-                transform.Translate(Vector3.left * Time.deltaTime * leftRightSpeed);
+                if (this.gameObject.transform.position.x > LevelBoundary.leftSide)
+                {
+                    transform.Translate(Vector3.left * Time.deltaTime * leftRightSpeed);
+                }
             }
-        }
-        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
-        {
-            if (this.gameObject.transform.position.x < LevelBoundary.rightSide)
+            if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
             {
-                transform.Translate(Vector3.left * Time.deltaTime * leftRightSpeed * -1);
+                if (this.gameObject.transform.position.x < LevelBoundary.rightSide)
+                {
+                    transform.Translate(Vector3.left * Time.deltaTime * leftRightSpeed * -1);
+                }
             }
         }
     }
