e3e8419d3458a89c14fa398b3273408f63b0f3f2
pandabunnz <115520957+pandabunnz@users.noreply.github.com>
2022-12-01 00:05:38 -0800

    game over screen

--- Peach's Turn To Run/Assets/Scenes+Scripts/Level/ObstacleCollision.cs
+++ Peach's Turn To Run/Assets/Scenes+Scripts/Level/ObstacleCollision.cs
@@ -6,11 +6,13 @@ public class ObstacleCollision : MonoBehaviour
 {
     public GameObject thePlayer;
     public AudioSource crash;
+    public GameObject levelControl;
 
     void OnTriggerEnter(Collider other)
     {
         this.gameObject.GetComponent<BoxCollider>().enabled = false;
         thePlayer.GetComponent<PlayerController>().enabled = false;
         crash.Play();
+        levelControl.GetComponent<EndRunSequence>().enabled = true;
     }
 }
