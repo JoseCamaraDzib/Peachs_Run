de1154c9c65f60a58460510791a314d53671fe9f
pandabunnz <115520957+pandabunnz@users.noreply.github.com>
2022-11-30 21:12:59 -0800

    serialize field

--- Peach's Turn To Run/Assets/Scenes+Scripts/Player/PlayerController.cs
+++ Peach's Turn To Run/Assets/Scenes+Scripts/Player/PlayerController.cs
@@ -4,25 +4,11 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    /*[SerializeField] public float speed;
-    public Rigidbody rb;
+    [SerializeField] 
+    public float moveSpeed = 15;
 
-    float horizontalInput;
-    public float horizontalMultiplier = 2;
-    private void FixedUpdate()
-    {
-        Vector3 forwardMove = transform.forward * speed * Time.fixedDeltaTime;
-        Vector3 horizontalMove = transform.right * horizontalInput * speed * Time.fixedDeltaTime;
-        rb.MovePosition(rb.position + forwardMove + horizontalMove);
-    }
-
-    void Update()
-    {
-        horizontalInput = Input.GetAxis("Horizontal");
-    }*/
-
-    public float moveSpeed = 3;
-    public float leftRightSpeed = 4;
+    [SerializeField]
+    public float leftRightSpeed = 8;
 
     private void Update()
     {
