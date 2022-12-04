922aba8b9072dc7bbb4e9f5ad94af39a9ca454c0
yutaroee <vagas@uci.edu>
2022-12-04 15:55:09 -0800

    Score Script and UI added (not working yet)

---
+++ Peach's Turn To Run/Assets/Scenes+Scripts/Score Manager/ScoreManager.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreManager : MonoBehaviour
+{
+    public Text ScoreText;
+    public Text HighScoreText;
+    public float ScoreCount;
+    public float HighScoreCount;
+    public float ptspersec;
+    public bool scoreinc;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(scoreinc)
+        {
+            ScoreCount += ptspersec * Time.deltaTime;
+        }
+        
+        if(ScoreCount > HighScoreCount)
+        {
+            HighScoreCount = ScoreCount;
+        }
+
+        ScoreText.text = "Score: " + Mathf.Round(ScoreCount);
+        HighScoreText.text = "High Score: " + Mathf.Round(HighScoreText);
+    }
+}
